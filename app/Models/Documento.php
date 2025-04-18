c885423b82eea45e13d06bfae9398e5719c9e6cf
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-04-18 15:22:09 -0300

    Criação de Models

---
+++ app/Models/Documento.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Documento extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['url', 'horas_in', 'status', 'comentario', 'horas_out'];
+}
