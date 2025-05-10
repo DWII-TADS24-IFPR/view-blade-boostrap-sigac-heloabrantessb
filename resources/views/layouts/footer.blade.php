a1113cc8e51d388134ea5fdec31f6494a28e90f6
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-05-09 22:58:00 -0300

    layouts base

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
+<div>
+    <footer class="d-flex flex-column align-items-center">
+        <p>
+             SIGAC - Sistema de Gerenciamento de Atividades Complementares &copy; {{ date('Y') }}
+        </p>
+        <p>All rights reserved.</p>
+    </footer>
+</div>
